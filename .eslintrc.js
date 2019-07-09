8db64d6e82334be1ebf798ea70eded4d1bf9919d
Grigorii Horos <horosgrisa@gmail.com>
2019-07-09 23:37:18 +0300

    Fix format

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,36 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
   extends: [
-    "eslint:recommended",
-    "airbnb-base",
-    "plugin:unicorn/recommended",
-    "plugin:promise/recommended",
-    "standard-jsdoc"
+    'eslint:recommended',
+    'airbnb-base',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+    'standard-jsdoc',
   ],
   plugins: [
-    "no-loops",
-    "unicorn",
-    "async-await",
-    "prefer-object-spread",
-    "promise",
-    "security"
+    'no-loops',
+    'unicorn',
+    'async-await',
+    'prefer-object-spread',
+    'promise',
+    'security',
   ],
   env: {
     browser: true,
     es6: true,
-    mocha:true
+    mocha: true,
   },
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   rules: {
-    "unicorn/filename-case": 1,
-    "unicorn/import-index": 0,
-    "import/extensions": 0,
-    "prefer-object-spread/prefer-object-spread": 2,
-  }
+    'unicorn/filename-case': 1,
+    'unicorn/import-index': 0,
+    'import/extensions': 0,
+    'prefer-object-spread/prefer-object-spread': 2,
+  },
 };
